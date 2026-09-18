f35999e7de72d9141d1c83798196522d664e8195
anubhavsingh0079 <anubhavsingh88141@gmail.com>
2026-09-18 17:43:48 +0530

    command

---
+++ .kilo/index.js
@@ -0,0 +1,42 @@
+alert("JavaScript is working!");
+
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+   // button.addEventListener("click", () => {
+   //     display.value += button.textContent;
+    //    buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const value = button.textContent;
+
+        if (value === "C") {
+
+            display.value = "";
+
+        } 
+        else if (value === "=") {
+            let expression = display.value;
+
+            expression = expression.replaceAll("x","*");
+            expression = expression.replaceAll("÷", "/");
+            expression = expression.replaceAll("−", "-");
+            display.value = eval(display.value);
+
+        } 
+        else {
+
+            display.value += value;
+
+        }
+
+    });
+
+});
+    //});
+//});
+
+
+
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ or
 git add . -- all
 git commit -m "message"
 git push -u origin main "msg"
+## Calculator 
